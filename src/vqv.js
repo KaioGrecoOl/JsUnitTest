732463b37c5a41e0668bd684ed40506cf1d11419
Kaio G <kaiogreco91@gmail.com>
2022-01-17 22:25:56 -0300

    requisito 3 vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+// Consegui resolver o requisito após leitura do conteúdo do site https://medium.com/@adsonrocha/template-strings-com-es6-c3030dec791
+
+const vqv = (name, age) => {
+  if (name === undefined || age === undefined) {
+    return undefined;
+  } 
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos, trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
