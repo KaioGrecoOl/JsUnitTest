f9ed685ef6b664db4a2cecd6d218238d7f474e6f
Kaio G <kaiogreco91@gmail.com>
2022-01-18 18:24:43 -0300

    conclui requsito 3

--- src/vqv.js
+++ src/vqv.js
@@ -21,9 +21,10 @@
 const vqv = (name, age) => {
   if (name === undefined || age === undefined) {
     return undefined;
-  } 
+  }
   return `Oi, meu nome é ${name}!
-Tenho ${age} anos, trabalho na Trybe e mando muito em programação!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
 
